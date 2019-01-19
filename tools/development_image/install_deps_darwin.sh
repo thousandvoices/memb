aef9a07331b64f2d7c1615ecbe1584c06c1ed0d6
Vladimir Bazhenov <theonlything123+321@gmail.com>
2019-01-19 09:38:32 +0300

    One more fix

--- tools/development_image/install_deps_darwin.sh
+++ tools/development_image/install_deps_darwin.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-brew upgrade python3 cmake boost flatbuffers
+install_or_upgrade {
+    if brew ls --versions "$1" >/dev/null; then
+        HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1"
+    else
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
+    fi
+}
+
+REQUIRED_PACKAGES="python3 cmake boost flatbuffers"
+
+for package in "${PREQUIRED_PACKAGES[@]}" ; do
+    install_or_upgrade "$package"
+done
