f07ba83afd88251e553da79b2c4b50ae52af7c58
Vladimir Bazhenov <theonlything123+321@gmail.com>
2019-03-28 08:23:47 +0300

    flatbuffers download

--- tools/development_image/install_deps_windows.ps1
+++ tools/development_image/install_deps_windows.ps1
@@ -1,8 +1,15 @@
 $flatbuffers_version = '1.9.0'
 
-$flatbuffers_url = "https://github.com/google/flatbuffers/releases/download/v$flatbuffers_version/flatc_windows_exe.zip"
-$flatbuffers_archive = "flatc_windows_exe.zip"
+$flatbuffers_url = "https://github.com/google/flatbuffers/archive/v$flatbuffers_version.zip"
+$flatc_url = "https://github.com/google/flatbuffers/releases/download/v$flatbuffers_version/flatc_windows_exe.zip"
+$flatbuffers_archive = "flatbuffers.zip"
+$flatc_archive = "flatc_windows_exe.zip"
 $flatbuffers_install_path = "C:\flatbuffers"
 
 Start-FileDownload $flatbuffers_url -FileName $flatbuffers_archive
 Expand-Archive $flatbuffers_archive -DestinationPath $flatbuffers_install_path
+Remove-Item $flatbuffers_archive
+
+Start-FileDownload $flatc_url -FileName $flatc_archive
+Expand-Archive $flatc_archive -DestinationPath $flatbuffers_install_path
+Remove-Item $flatc_archive
