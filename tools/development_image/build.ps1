4640c9788b0f8e8c7f78435d26c979df9d3ae8bf
Vladimir Bazhenov <theonlything123+321@gmail.com>
2019-03-28 09:33:16 +0300

    Fix

--- tools/development_image/build.ps1
+++ tools/development_image/build.ps1
@@ -1,6 +1,11 @@
-cmake -A x64 $env:APPVEYOR_BUILD_FOLDER
-cmake --build .
-$env:BOOST_TEST_LOG_LEVEL = "test_suite"
-
 pip install wheel
 pip wheel $env:APPVEYOR_BUILD_FOLDER
+
+$tests_build_dir = $env:APPVEYOR_BUILD_FOLDER/build
+New-Item -ItemType directory -Path $tests_build_dir
+Set-Location $tests_build_dir
+
+cmake -A x64 -DCMAKE_BUILD_TYPE=Debug $env:APPVEYOR_BUILD_FOLDER
+cmake --build .
+$env:BOOST_TEST_LOG_LEVEL = "test_suite"
+Debug/test_runner.exe
