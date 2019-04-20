1da06b4585b54eec44527050e0e740c18d82b6d8
thousandvoices <theonlything123+321@gmail.com>
2019-04-20 08:55:08 +0300

    Inline frenquently called functions (#8)

--- src/huffman_decoder.h
+++ src/huffman_decoder.h
@@ -1,55 +1,11 @@
 #pragma once
 
 #include "huffman_decoder_generated.h"
-#include "bit_stream.h"
-#include "prefix_code.h"
+#include "huffman_table_decoder.h"
 
-#include <unordered_map>
+#include <vector>
 
 namespace memb {
-
-struct CodeInfo {
-    uint8_t key;
-    size_t length;
-};
-
-std::unordered_map<uint8_t, PrefixCode> createCanonicalPrefixCodes(
-    const std::vector<CodeInfo>& codeLengths);
-
-class HuffmanTableDecoder {
-public:
-    struct DecodeState {
-        BitStreamReader reader;
-        size_t bitsToPull;
-    };
-
-    HuffmanTableDecoder(
-        const std::vector<uint8_t>& keys,
-        const std::vector<uint32_t>& sizeOffsets,
-        size_t maxDirectDecodeBitLength);
-
-    DecodeState decode(const uint8_t* source, size_t sourceSize) const;
-    uint8_t next(DecodeState& state) const;
-
-private:
-    uint16_t baseOffset(const std::unordered_map<uint8_t, PrefixCode>& codes, uint8_t key);
-    
-    struct DirectDecodeData {
-        uint8_t key;
-        uint8_t bitsCount;
-    };
-
-    struct IndirectDecodeData {
-        size_t offset;
-        uint8_t maxBitsCount;
-    };
-
-    size_t maxDirectDecodeBitLength_;
-    uint32_t decodeTableBitMask_;
-    std::vector<DirectDecodeData> decodeTable_;
-    std::vector<IndirectDecodeData> indirectOffsetsTable_;
-    std::vector<DirectDecodeData> indirectDecodeTable_;
-};
 
 class HuffmanDecoder {
 public:
--- src/prefix_code.h
+++ src/prefix_code.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+#include <vector>
 #include <cstddef>
 #include <cstdint>
 
@@ -9,5 +11,13 @@ struct PrefixCode {
     uint16_t code;
     size_t bitsCount;
 };
+
+struct CodeInfo {
+    uint8_t key;
+    size_t length;
+};
+
+std::unordered_map<uint8_t, PrefixCode> createCanonicalPrefixCodes(
+    const std::vector<CodeInfo>& codeLengths);
 
 }
